287650728c4c505f877bd99eed3ed8de90443ace
dkhundley <david.k.hundley@gmail.com>
2023-04-14 10:42:32 -0500

    Completing basic chat script

--- src/chat.py
+++ src/chat.py
@@ -1,4 +1,5 @@
 # Importing the necessary Python libraries
+import os
 import re
 import yaml
 import openai
@@ -16,26 +17,115 @@ with open('../keys/openai-keys.yaml') as f:
 openai.organization = keys_yaml['ORG_ID']
 openai.api_key = keys_yaml['API_KEY']
 
-# Establishing a bit of regex to catch social security numbers
-ssn_regex = r'\b(?!000)(?!666)(?!9\d{2})\d{3}[-]?(?!00)\d{2}[-]?(?!0000)\d{4}\b'
 
 
-## SCRIPT INSTANTIATION
+## HELPER FUNCTIONS
 ## ---------------------------------------------------------------------------------------------------------------------
-if __name__ == "__main__":
-    print('Welcome to my ChatGPT Python script! Enter a prompt to begin the conversation.')
+def initiate_chat_flow():
+    '''
+    Initiates a new chat flow
 
-    user_prompt = input('What would you like to ask?\n')
-    print(user_prompt)
+    Inputs:
+        - N/A
 
-    next_choice = [
+    Returns:
+        - chat_flow (list): A newly initiated chat flow
+    '''
+
+    chat_flow = [
+        {'role': 'system', 'content': 'You are a classy butler, like Alfred from Batman.'}
+    ]
+
+    return chat_flow
+
+def check_sensitive_data(user_prompt):
+    '''
+    Checks the user's prompt to see if any sensitive information has been pass in via the prompt
+
+    Inputs:
+        - user_prompt (str): The user's inputted prompt
+
+    Returns:
+        - has_sensitive_data (bool): A boolean value indicating if the prompt contains sensitive data
+    '''
+
+    # Establishing a bit of regex to catch social security numbers
+    ssn_regex = r'\b(?!000)(?!666)(?!9\d{2})\d{3}[-]?(?!00)\d{2}[-]?(?!0000)\d{4}\b'
+
+def prompt_next_choice():
+    '''
+    Prompts the user to continue the current conversation, start a new conversation, or end the program
+
+    Inputs:
+        - N/A
+
+    Returns:
+        - next_action (str): The choice selected by the user
+    '''
+
+    # Setting the list of options that the user can select from
+    user_choices = [
         inquirer.List(
-            'selected_choice',
+            'user_choices',
             message = 'Would you like to continue the conversation?',
             choices = ['Yes', 'Start New Conversation', 'End Program']
         )
     ]
 
-    selected_choice = inquirer.prompt(next_choice)
+    # Retreiving the selected option by the user
+    next_action = inquirer.prompt(user_choices)['user_choices']
 
-    print(selected_choice['selected_choice'])
+    return next_action
+
+
+## SCRIPT INSTANTIATION
+## ---------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    # Printing a welcome statement
+    print('Welcome to my ChatGPT Python script! Enter a prompt to begin the conversation.')
+
+    # Starting an initial chat flow
+    chat_flow = initiate_chat_flow()
+
+    # Starting a reiterating loop for the prompts
+    while True:
+
+        # Retrieving the prompt input from the user
+        user_prompt = input('What would you like to ask?\n')
+
+        # Checking the prompt for any sensitive data
+        has_sensitive_data = check_sensitive_data(user_prompt)
+
+        # Prompting the user to submit a new prompt without sensitive data if sensitive data is present
+        if has_sensitive_data:
+            print('Your prompt appears to have sensitive data in the body of the text. Please remove this sensitive data and submit a new prompt.')
+            continue
+
+        # Appending the user prompt to the chat flow
+        chat_flow.append({'role': 'user', 'content': user_prompt})
+
+        # Obtaining the response from the API
+        chat_response = openai.ChatCompletion.create(
+            model = 'gpt-3.5-turbo',
+            messages = chat_flow
+        )
+
+        # Printing ChatGPT's response back to the user
+        print(f"\nChatGPT's response: {chat_response['choices'][0]['message']['content']}\n")
+
+        # Prompting the user if they would like to continue the current chat, start a new one, or end the program
+        next_action = prompt_next_choice()
+
+        # Taking the appropriate action based on the user's next desired action
+        if next_action == 'Yes':
+
+            # Appending the result of the chat response to the chat flow for continued conversation
+            chat_flow.append({'role': 'assistant', 'content': chat_response['choices'][0]['message']['content']})
+
+        elif next_action == 'Start New Conversation':
+
+            # Re-initiating the chat flow
+            chat_flow = initiate_chat_flow()
+
+        elif next_action == 'End Program':
+            exit(0)
